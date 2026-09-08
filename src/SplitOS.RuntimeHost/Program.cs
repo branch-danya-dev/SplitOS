67a0d9c0c32c0c8b7c7bf2265a4cf357c18accf9
Daniel Rogulin <68234412+branch-danya-dev@users.noreply.github.com>
2026-09-08 15:40:27 +0300

    delivery: wire online entitlement runtime access evaluator

--- src/SplitOS.RuntimeHost/Program.cs
+++ src/SplitOS.RuntimeHost/Program.cs
@@ -4,6 +4,7 @@ using SplitOS.Persistence.ProtectedSecrets;
 using SplitOS.Persistence.Projection;
 using SplitOS.Persistence.User;
 using SplitOS.RuntimeHost;
+using SplitOS.RuntimeHost.ProductIdentity;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddSingleton<RuntimeUiCallerValidator>();
@@ -16,8 +17,9 @@ builder.Services.AddSingleton<ProjectionStore>();
 builder.Services.AddSingleton<IAccountSecretStore, DpapiAccountSecretStore>();
 builder.Services.AddSingleton<IWindowsUserContext, WindowsUserContext>();
 builder.Services.AddSingleton<AccountAssociationCoordinator>();
+builder.Services.AddSingleton<OnlineEntitlementEvidenceState>();
 builder.Services.AddSingleton<MachineStateClient>();
-builder.Services.AddSingleton<IRuntimeAccessEvaluator, DeterministicFreeRuntimeAccessEvaluator>();
+builder.Services.AddSingleton<IRuntimeAccessEvaluator, OnlineEntitlementRuntimeAccessEvaluator>();
 builder.Services.AddHostedService<RuntimeStateCoordinator>();
 builder.Services.AddHostedService<RuntimeUiPipeService>();
 builder.Services.AddHostedService<BrokerHealthMonitor>();
