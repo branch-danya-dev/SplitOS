3968206997716ba9c2514e91b5afee6ee3358402
Daniel Rogulin <68234412+branch-danya-dev@users.noreply.github.com>
2026-09-10 02:49:29 +0300

    feat: register managed service verification boundary

--- src/SplitOS.Broker.Service/Program.cs
+++ src/SplitOS.Broker.Service/Program.cs
@@ -8,12 +8,14 @@ builder.Services.AddWindowsService(options => options.ServiceName = "SplitOS Pri
 builder.Services.AddSingleton<MachineStateStore>();
 builder.Services.AddSingleton<ModeMutationFenceStore>();
 builder.Services.AddSingleton<ModePreMutationEvidenceStore>();
+builder.Services.AddSingleton<ModeVerificationEvidenceStore>();
 builder.Services.AddSingleton<ModeTransitionActionJournalStore>();
 builder.Services.AddSingleton<BrokerModeMutationFenceBoundary>();
 builder.Services.AddSingleton<IManagedServiceCatalog, ReleaseManagedServiceCatalog>();
 builder.Services.AddSingleton<IManagedServiceAdapter, WindowsManagedServiceAdapter>();
 builder.Services.AddSingleton<BrokerManagedServiceSnapshotExecutor>();
 builder.Services.AddSingleton<BrokerManagedServicePolicyExecutor>();
+builder.Services.AddSingleton<BrokerManagedServiceVerificationExecutor>();
 builder.Services.AddSingleton<BrokerCallerValidator>();
 builder.Services.AddSingleton<BrokerMessageHandler>();
 builder.Services.AddHostedService<BrokerPipeService>();
