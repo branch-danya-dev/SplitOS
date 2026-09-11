abd88e9a5543c20b2cce8c6e1443339cbf0af52a
Daniel Rogulin <68234412+branch-danya-dev@users.noreply.github.com>
2026-09-12 01:22:16 +0300

    register persistent display selector resolver

--- src/SplitOS.RuntimeHost/Program.cs
+++ src/SplitOS.RuntimeHost/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddSingleton<IDisplayConfigQuery, WindowsDisplayConfigQuery>();
 builder.Services.AddSingleton<IDisplaySnapshotReader, DisplaySnapshotReader>();
 builder.Services.AddSingleton<IDisplayNativeTargetApplier, WindowsDisplayConfigTargetApplier>();
 builder.Services.AddSingleton<DisplayTargetApplyCoordinator>();
+builder.Services.AddSingleton<PersistentDisplaySelectorResolver>();
 builder.Services.AddSingleton<UserStateStore>();
 builder.Services.AddSingleton<IUserAccountAssociationStore>(services => services.GetRequiredService<UserStateStore>());
 builder.Services.AddSingleton<IUserAccountAssociationSignOutStore, UserAccountAssociationSignOutStore>();
